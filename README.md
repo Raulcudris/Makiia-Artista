d94fc3739631caba908ae6e7db23d71c776c5c82
Raul Cudris <raulcudriz@hotmail.com>
2023-02-18 10:43:51 -0500

    Primer Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# MakiiaArtistas
+HomePages Makiia Artist
+
+Makiia-HomePage-Artist
+
+-Microservicio  HOME PAGE ARTIST
+  
+  1.Datos Informacion del Artista(Biografia)
+    *Direccion
+    *Redes Sociales
+    *Link Foto Perfil    
+  2.Datos de la Agenda
+  3.Datos de las Tarifas
+  4.Datos del Carrusell
---
+++ homepage-artist/src/main/java/com/makiia/modules/bus/contracts/IjpaDataProviders.java
@@ -0,0 +1,21 @@
+package com.makiia.modules.bus.contracts;
+
+import com.makiia.crosscutting.domain.model.EntyRecmaetarivalorDto;
+import com.makiia.crosscutting.exceptions.Main.EBusinessException;
+
+import java.util.List;
+
+public interface  IjpaDataProviders<T> {
+
+    List<T> getAll() throws EBusinessException;
+
+    T get(String id) throws EBusinessException;
+
+    T save(T dto) throws EBusinessException;
+
+    List<T> save(List<T> dto) throws EBusinessException;
+
+    T update(String id, T dto) throws EBusinessException;
+
+    void delete(String id) throws EBusinessException;
+}
